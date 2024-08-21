eca91a98911c15271d63a5daefe8985a3ce6c1cb
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-08-21 23:08:39 +0300

    exercises 11.10-11.11

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
