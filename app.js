cc54c8c014c859ac2e60f15caa198694ff0f93ee
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2024-08-22 13:41:13 +0300

    exercise 11.12 testing broken state

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
-  this_causes_error
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
